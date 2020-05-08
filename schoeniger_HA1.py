66a78235691194c36c9090740040573941e512c4
Unknown <schoeniger@eeg.tuwien.ac.at>
2020-05-08 15:44:56 +0200

    Add Python codebase for the homework #1

---
+++ schoeniger_HA1.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Oct  8 17:25:00 2018
+
+@author: schoeniger
+"""
+
+# Importieren der benötogten Module
+import numpy as np
+import matplotlib.pyplot as plt
+
+# Parameter nicht als globale Variablen definieren, sondern in der Funktion
+
+# Definieren der Funktion
+def yamamura(theta, Y_0 = 0.11, f = 1.9, b = 0.19):
+    '''Yamamura equation to calculate the sputtering yield'''
+ 
+    Y = Y_0*(np.cos(theta))**(-f)*np.exp(b*(1-1/np.cos(theta)))
+    return Y
+
+# Plotten der Funktion
+xvals = np.linspace(0., 90.)
+yvals = yamamura(np.radians(xvals))
+
+plt.title('Sputtering yield according to the Yamamura formula')
+plt.plot(xvals, yvals, 'r+-', label='Yamamura(theta)')
+plt.legend()
+plt.show()
