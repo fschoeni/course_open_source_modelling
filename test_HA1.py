967599f8ce2755b055f975c7a3c642e18f142e3f
Daniel Huppmann <dh@dergelbesalon.at>
2020-05-22 15:01:53 +0200

    Try making stickler and travis fail

--- test_HA1.py
+++ test_HA1.py
@@ -3,6 +3,8 @@ from HA1 import roses
 
 def test_yamamura():
 	assert yamamura(theta = 0.11, Y_0 = 0, f = 1.9, b = 0.19) == 0
+  
+  
     
 def test_roses():
-    assert roses(0, 90) == 1
+    assert roses(0, 90) == 2
